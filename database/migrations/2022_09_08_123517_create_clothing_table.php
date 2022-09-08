21bd75ac624cd00551386e3f20fc4099782c2688
FedEvg <fedchenko703@gmail.com>
2022-09-08 17:04:08 +0300

    db migrate

---
+++ database/migrations/2022_09_08_123517_create_clothing_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clothing', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price')->nullable();
+            $table->foreignId('category_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('brand_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('quantity')->nullable();
+            $table->unsignedSmallInteger('availability_id')->default(0);
+            $table->foreignId('size_id')->nullable()->constrained();
+            $table->integer('discount')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clothing');
+    }
+};
